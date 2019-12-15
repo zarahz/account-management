b4c93dd2684256b041d27c7af596bfa1ed29da56
alexander <alexander.zwermann@horizon-alpha.com>
2019-12-15 20:06:04 +0100

    fixed flow errors

--- libdefs.js
+++ libdefs.js
@@ -1,23 +1,36 @@
 declare module 'react' {
     declare var exports: any;
 }
-
 declare module '@material-ui/core/CircularProgress' {
     declare var exports: any;
 }
-
 declare module '@material-ui/core/Typography' {
     declare var exports: any;
 }
-
-declare module 'material-ui/core/Snackbar' {
+declare module '@material-ui/core/Snackbar' {
     declare var exports: any;
 }
-
-declare module 'material-ui/core/SnackbarContent' {
+declare module '@material-ui/core/SnackbarContent' {
     declare var exports: any;
 }
-
-declare module 'material-ui/core/IconButton' {
+declare module '@material-ui/core/IconButton' {
+    declare var exports: any;
+}
+declare module  '@material-ui/icons/Close' {
+    declare var exports: any;
+}
+declare module '@material-ui/core/styles' {
+    declare var exports: any;
+}
+declare module 'redux' {
+    declare var exports:any;
+}
+declare module 'crypto-js' {
+    declare var exports: any;
+}
+declare module 'react-redux' {
+    declare var exports: any;
+}
+declare module 'redux-thunk' {
     declare var exports: any;
 }
