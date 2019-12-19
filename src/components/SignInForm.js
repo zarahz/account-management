7a2d75839174df2b4530c17d01571ee48766ead9
alexander <alexander.zwermann@horizon-alpha.com>
2019-12-19 11:09:56 +0100

    removed duplicated onSubmit

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -34,7 +34,7 @@ export class SignInForm extends React.Component {
   render () {
     return (
       <div className="FormCenter">
-        <form onSubmit={this.handleSubmit} className="FormFields" onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className="FormFields">
           <div className="FormField">
             <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
             <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
