159f2b50b873befe719ea28a624f076a2c5b4e6f
onum <>
2020-01-29 16:42:26 +0100

    implemented test for all apis

---
+++ src/services/DecoderService.test.js
@@ -0,0 +1,34 @@
+import DecoderService from './DecoderService.js';
+
+test('Should return the user', async () => {
+  const decoderService = new DecoderService();
+  const userModel = {
+    titel: 'sir',
+    firstname: 'john',
+    lastname: 'doe',
+    username: 'johndoe3',
+    email: 'john35@doe.com',
+    password: 'johndoe',
+    fieldOfActivity: 'computer science',
+    researchInterest: ['AR'],
+    securityQuestion: 'In what town or city did you meet your spouse/partner?',
+    securityAnswer: 'Doe town'
+  };
+  const token = 'eyJhbGciOiJIUzI1NiJ9.IjVlMzE4M2RmNTMwZDZhYTVhODkyMGRlZSI.dc9DYAvJn6bd5Tom4bZIxGYWGvnKcmvtH8qwMlhFIcw';
+  const user = await decoderService.decode(token);
+  expect(user).toMatchObject({
+    id: '5e3183df530d6aa5a8920dee',
+    title: 'sir',
+    role: 'user',
+    firstname: 'john',
+    lastname: 'doe',
+    username: 'johndoe3',
+    email: 'john35@doe.com',
+    organisation: 'LMU',
+    fieldOfActivity: 'computer science',
+    researchInterest: [
+      'AR'
+    ],
+    eventbasedRole: []
+  });
+});
