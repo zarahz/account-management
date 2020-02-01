55ea15ed36b0dc2870955b284352d780ba8a57fa
alexander <alexander.zwermann@horizon-alpha.com>
2020-02-01 10:57:29 +0100

    more fixes

--- src/services/LoginService.test.js
+++
@@ -1,7 +0,0 @@
-import LoginService from './LoginService.js';
-
-test('Should return a token', async () => {
-  const loginService = new LoginService();
-  const token = await loginService.login('johndoe3', 'johndoe');
-  expect(token).toMatchObject({token: 'eyJhbGciOiJIUzI1NiJ9.IjVlMzE4M2RmNTMwZDZhYTVhODkyMGRlZSI.dc9DYAvJn6bd5Tom4bZIxGYWGvnKcmvtH8qwMlhFIcw'});
-});
